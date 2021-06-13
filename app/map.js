f3d1c838e3656d8e64e1b2c1d9264562dbe504c2
J-Guenther <jannik.guentheg@gmail.com>
2021-06-13 23:16:46 +0200

    added map interaction and styles

--- app/map.js
+++ app/map.js
@@ -6,7 +6,21 @@ import View from 'ol/View';
 import Feature from 'ol/Feature';
 import GeoJSON from 'ol/format/GeoJSON';
 import {Vector as VectorSource} from 'ol/source';
-import { borders } from './data';
+import Fill from 'ol/style/Fill';
+import Stroke from 'ol/style/Stroke';
+import Style from 'ol/style/Style';
+import Select from 'ol/interaction/Select';
+import {borders} from './data';
+
+const highlightStyle = new Style({
+    fill: new Fill({
+        color: 'rgba(255,255,255,0.7)',
+    }),
+    stroke: new Stroke({
+        color: '#3399CC',
+        width: 3,
+    }),
+});
 
 const vectorSource = new VectorSource({
     features: new GeoJSON().readFeatures(borders, {
@@ -19,11 +33,13 @@ const vectorLayer = new VectorLayer({
     source: vectorSource
 });
 
+const view = new View({
+    center: [0, 0],
+    zoom: 1,
+});
+
 const map = new Map({
-    view: new View({
-        center: [0, 0],
-        zoom: 1,
-    }),
+    view: view,
     layers: [
         new TileLayer({
             source: new OSM(),
@@ -31,4 +47,63 @@ const map = new Map({
         vectorLayer
     ],
     target: 'ol-map'
+});
+
+view.fit(vectorSource.getExtent());
+
+const selectionDisplay = document.getElementById('selectionDisplay');
+
+let selected = null;
+
+const selectSingleClick = new Select();
+
+map.addInteraction(selectSingleClick);
+selectSingleClick.on('select', function (e) {
+    selected?.setStyle(undefined);
+    selected = e.selected[0];
+    selectionDisplay.innerHTML = selected.get('GEN');
+});
+
+let hover = null;
+map.on('pointermove', function (e) {
+    if (hover !== null && hover !== selected) {
+        hover.setStyle(undefined);
+        hover = null;
+    }
+
+    map.forEachFeatureAtPixel(e.pixel, function (f) {
+        f.setStyle(highlightStyle);
+        hover = f;
+        return true;
+    });
+});
+
+const ul = document.createElement('ul');
+
+document.getElementById('areaContainer').appendChild(ul);
+
+const features = vectorSource.getFeatures();
+features.sort((a, b) => a.get('GEN').localeCompare(b.get('GEN')));
+
+features.forEach(feature => {
+    if (feature.get('GEN') !== 'Leer' && feature.get('GF') === 4) {
+        let li = document.createElement('li');
+        li.addEventListener("mouseenter", () => {
+            console.log('Mousenter ', feature.get('DEBKG_ID'));
+            feature.setStyle(highlightStyle);
+        });
+        li.addEventListener("mouseleave", () => {
+            if (feature !== selected) {
+                feature.setStyle(undefined);
+            }
+        });
+        li.addEventListener("click", () => {
+            selected?.setStyle(undefined);
+            feature.setStyle(highlightStyle);
+            selected = feature;
+            selectionDisplay.innerHTML = selected.get('GEN');
+        });
+        ul.appendChild(li);
+        li.innerHTML += feature.get('GEN');
+    }
 });
